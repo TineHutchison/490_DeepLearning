29367d7c7eaa16757bfd483073dca8c406c887bd
Tine Hutchison <Tine.Hutchison@VerizonDigitalMedia.com>
2017-10-25 10:18:51 -0700

    added zero_pad() to add a border around a given object

--- letter/zero_pad.py
+++ letter/zero_pad.py
@@ -6,13 +6,15 @@ def zero_pad(lettr):
     :param lettr: a Letter object
     :return: a new Letter object
     """
-    new_root = lettr.root + 2
-    new_array = [0 for i in range(new_root)]
+    new_root = int(lettr.root + 2)
+    new_array = [0 for _ in range(new_root)]
     for idx, pixel in enumerate(lettr.array):
-        if idx == 0 | idx == len(lettr.array) + 1:
+        if idx == 0:
             new_array.append(0)
         elif idx % lettr.root == 0:
-            new_array.extend([0,0])
+            new_array.extend([0, 0])
         new_array.append(pixel)
-    new_array.extend([0 for i in range(new_root + 1)])
+    new_array.extend([0 for _ in range(new_root + 1)])
+
+    return Letter(new_array, lettr.letter)
 
